8d338e2de3cbf6e5cd86202b547192c39ed0460f
Asxoxl <maite.godoy06@gmail.com>
2026-05-22 18:06:15 -0400

    Incorporación del método get
    Método agregado con la finalidad de poder consultar la información guardada por el constructor, en donde, también cumpla la función de filtrar cualquier carácter que no corresponda a una letra del abecedario ingles y pasarla a minúscula, evitando errores al momento de  buscar en el arreglo.

--- InventarioLetras.java
+++ InventarioLetras.java
@@ -1,15 +1,31 @@
-public class InventarioLetras {private int[] contadorDeCadaLetra; private int
-        cantidadDeApariciones; public InventarioLetras (String letrasRecibidas) {
-            this.contadorDeCadaLetra = new int[26];
-            this.cantidadDeApariciones = 0;
-            String analisisTexto
-            = ""; for (int i = 0; i < letrasRecibidas.length();i++) { char caracter =
-            letrasRecibidas.charAt(i);if ((caracter >= 'a' && caracter <= 'z') || (caracter >= 'A' &&
-            caracter <= 'Z')){ analisisTexto = analisisTexto + caracter; }
-            }
-            String transformarAminusculas = analisisTexto.toLowerCase();for(int i = 0; i <
-            transformarAminusculas.length(); i++) { char letra = transformarAminusculas.charAt(i); int
-            posicion = letra - 'a'; this.contadorDeCadaLetra[posicion]++; this.cantidadDeApariciones++; }
+public class InventarioLetras {
+    private int[] contadorDeCadaLetra;
+    private int cantidadDeApariciones;
+
+    public InventarioLetras(String letrasRecibidas) {
+        this.contadorDeCadaLetra = new int[26];
+        this.cantidadDeApariciones = 0;
+        String analisisTexto = "";
+        for (int i = 0; i < letrasRecibidas.length(); i++) {
+            char caracter =
+                    letrasRecibidas.charAt(i);
+            if ((caracter >= 'a' && caracter <= 'z') || (caracter >= 'A' && caracter <= 'Z')) {
+                analisisTexto = analisisTexto + caracter;
             }
         }
+        String transformarAminusculas = analisisTexto.toLowerCase();
+        for (int i = 0; i < transformarAminusculas.length(); i++) {
+            char letra = transformarAminusculas.charAt(i);
+            int posicion = letra - 'a';
+            this.contadorDeCadaLetra[posicion]++;
+            this.cantidadDeApariciones++;
+        }
+    }
+    public int get (char letra) {if (!((letra >= 'a' && letra <= 'z') || (letra >= 'A' && letra <= 'Z'))) {
+        throw new IllegalArgumentException();
+    }
+    char letraMinuscula = Character.toLowerCase(letra); int posicion = letraMinuscula - 'a';
+    return this.contadorDeCadaLetra[posicion];}
+}
+
 

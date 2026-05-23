106ef795bc4b8ece16e785b5c6f234d5d0caa618
Asxoxl <maite.godoy06@gmail.com>
2026-05-22 20:08:34 -0400

    Método set y validación de parámetros
    El método permite modificar la cantidad de veces que aparece una letra especifica y mantiene actualizado el contador total. por otras parte, nuevamente se agregaron para evitar errores en el arreglo, en caso de que el método get se pase por alto y así al momento de encontrar un valor negativo o un carácter que no corresponda al alfabético arroje IllegalArgumentException.

--- InventarioLetras.java
+++ InventarioLetras.java
@@ -21,11 +21,34 @@ public class InventarioLetras {
             this.cantidadDeApariciones++;
         }
     }
-    public int get (char letra) {if (!((letra >= 'a' && letra <= 'z') || (letra >= 'A' && letra <= 'Z'))) {
-        throw new IllegalArgumentException();
+
+    public int get(char letra) {
+        if (!((letra >= 'a' && letra <= 'z') || (letra >= 'A' && letra <= 'Z'))) {
+            throw new IllegalArgumentException();
+        }
+        char letraMinuscula = Character.toLowerCase(letra);
+        int posicion = letraMinuscula - 'a';
+        return this.contadorDeCadaLetra[posicion];
     }
-    char letraMinuscula = Character.toLowerCase(letra); int posicion = letraMinuscula - 'a';
-    return this.contadorDeCadaLetra[posicion];}
+
+    public void set(char letra, int valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("El valor es negativo");
+        }
+        char letraMinuscula = Character.toLowerCase(letra);
+        if (letraMinuscula >= 'a' && letraMinuscula <= 'z') {
+            int posicion = letraMinuscula - (int) 'a';
+
+            int cantidadInicial = this.contadorDeCadaLetra[posicion];
+            this.cantidadDeApariciones = this.cantidadDeApariciones - cantidadInicial;
+            this.contadorDeCadaLetra[posicion] = valor;
+            this.cantidadDeApariciones = this.cantidadDeApariciones + valor;
+        } else {
+            throw new IllegalArgumentException("No es una letra valida");
+        }
+    }
 }
+
+
 
 
